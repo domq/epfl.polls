a214034b0509fe203d7f72be4635991cd2a6f933
Dominique Quatravaux <dominique@quatravaux.org>
2014-12-12 14:03:58 +0100

    Tentatives avec redis

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var redis = require("redis"),
+client = redis.createClient();
+
 var express = require('express');
 var router = express.Router();
 
@@ -7,7 +10,26 @@ router.get('/', function(req, res) {
 });
 
 router.get('/admin', function(req, res) {
-  res.render('admin', { title: 'Admin' });
+  var dboutput="";
+  function renderNow() {
+    res.render('admin', { title: 'Admin', databaseContents: dboutput });
+  }
+  client.keys("*", function (err, keys) {
+    var doneCount = 0;
+    for(var i = 0, len = keys.length; i < len; i++) {
+      dboutput +=keys[i];
+
+      client.get(keys[i], function (err, val) {
+        dboutput += val;
+        doneCount += 1;
+        if (doneCount == keys.length) {
+          renderNow();
+        }
+      });
+
+    }
+    //client.quit();
+  });
 });
 
 module.exports = router;
