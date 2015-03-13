cf833f471aa9dff3f81e9c6126b4f6b7887f21ed
ponsfrilus <ponsfrilus@gmail.com>
2015-03-13 13:40:54 +0100

    Footer responsif
    Pas trop shotgun débuggé mais encore un peu

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,9 @@ var fs = require("fs");
 
 module.exports = function(app){
     fs.readdirSync(__dirname).forEach(function(file) {
-        if (file == "router.js") return;
-        var name = (file === "index.js" ? "" : file.substr(0, file.indexOf('.')));
-        var route = require('./' + name);
-        app.use('/' + name, route.router);
+        if (file === "router.js") return;
+        var route = require('./' + file);
+        var baseUrl = (file === "index.js" ? "" : file.substr(0, file.indexOf('.')));
+        app.use('/' + baseUrl, route.router);
     });
-}
-
+};
