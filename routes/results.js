4dd85c4aba053fb952658ef4ad069968ec809fbe
ponsfrilus <ponsfrilus@gmail.com>
2015-03-20 13:41:44 +0100

    Footer responsif
    Pas trop shotgun débuggé mais encore un peu

--- routes/results.js
+++ routes/results.js
@@ -3,15 +3,63 @@ var router = express.Router();
 var tequila = require('../lib/tequila');
 var db = require('../lib/db');
 
-
 router.get('/', function(req, res) {
+    function whatToDoWithTheResults(results) {
+        console.log(results);
+        res.render('result', { title: 'Results', results: results });
+    }
+    tallyResults(whatToDoWithTheResults);
+});
+
+module.exports.router = router;
+
+function tallyResults(done) {
     db.pollsAnswer.mapReduce(
-        function(){emit(this.order[0], 1)},
-        function(key,values){return Array.sum(values)},
-        {out: {inline: 1}},
+        function(){
+
+            for (var i = this.order.length-1; i >= 0; i--) {
+                emit(this.order[i], this.order.length-i);
+            }
+
+
+        },
+        function(key,values){
+            return Array.sum(values)
+        },
+        {
+            filter: {},
+            out: {inline: 1}
+        },
         function (err, mapReduced) {
-            res.render('result', { title: 'Results', results: mapReduced });
+            done(mapReduced);
         }
     )
-});
-module.exports.router = router;
+
+}
+
+function off(){
+
+
+db.pollsAnswer.mapReduce(
+    function(){
+
+        for (var i = this.order.length-1; i >= 0; i--) {
+            emit(this.order[i], this.order.length-i);
+        }
+
+
+    },
+    function(key,values){
+        return Array.sum(values)
+    },
+    {
+        filter: {},
+        out: {inline: 1}
+    },
+    function (err, mapReduced) {
+        console.log(mapReduced);
+        res.render('result', { title: 'Results', results: mapReduced });
+    }
+)
+
+}
